09d62cf8b6065561d3fc4225cd4937530e7b8b49
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-06-19 03:14:30 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,10 @@
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 require('dotenv').config();
 
-// The command name is 'yukisettings'
 const commands = [
     new SlashCommandBuilder()
-        .setName('yukisettings') // This is the command you'll use!
-        .setDescription('Configure Kohana Yuki bot settings (Admin only in servers, personal settings in DMs)')
+        .setName('yukisettings')
+        .setDescription('Configure bot settings (Admin only in servers, personal settings in DMs)')
         .toJSON()
 ];
 
@@ -14,17 +13,12 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 (async () => {
     try {
         console.log('Starting deployment of application commands...');
-
-        // Deploy commands globally
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands }
         );
-
         console.log('Successfully deployed application commands globally.');
     } catch (error) {
         console.error('Error deploying commands:', error);
     }
 })();
-
-
